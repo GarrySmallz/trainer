5d9446e16c03e8310e76ae5656797a2a60939760
Garry Smallz <felixb1996@gmx.de>
2026-07-22 14:07:18 +0200

    fix: delete condition in DataSourceConfig, otherwise application does not start

--- src/main/java/de/trainer/config/GarminDataSourceConfig.java
+++ src/main/java/de/trainer/config/GarminDataSourceConfig.java
@@ -2,7 +2,6 @@ package de.trainer.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,7 +10,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
-@ConditionalOnExpression("!'${garmin.activities.datasource.url:}'.trim().isEmpty()")
 public class GarminDataSourceConfig {
 
     @Value("${garmin.activities.datasource.url}")
