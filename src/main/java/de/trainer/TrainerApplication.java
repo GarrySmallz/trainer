40451f8c70ba064cedf870fa26007cae3d6185bc
Garry Smallz <felixb1996@gmx.de>
2026-07-24 18:34:55 +0200

    feat: enable Configuration Properties for api key

--- src/main/java/de/trainer/TrainerApplication.java
+++ src/main/java/de/trainer/TrainerApplication.java
@@ -1,11 +1,14 @@
 package de.trainer;
 
+import de.trainer.security.ApiKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableConfigurationProperties({ApiKeyProperties.class})
 public class TrainerApplication {
 
     public static void main(String[] args) {
