4250fff0859b77a498b180818b909a59c7443ffb
Garry Smallz <felixb1996@gmx.de>
2026-09-14 15:03:24 +0200

    feat: configure filterchain to use oauth

--- src/main/java/de/trainer/security/SecurityConfig.java
+++ src/main/java/de/trainer/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
@@ -19,7 +20,8 @@ import java.util.List;
 public class SecurityConfig{
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
         http.cors(cors ->
                 cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
@@ -34,8 +36,11 @@ public class SecurityConfig{
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt -> {}));
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})
+                        .protectedResourceMetadata(metadata -> metadata
+                                .protectedResourceMetadataCustomizer(builder -> builder
+                                        .authorizationServer(issuerUri))));
         return http.build();
     }
 }
